2a2110d7990ec52d91b3ad8468e0a0481749eee8
Viktor Marynyuk <viktor.marynyuk@gmail.com>
2020-10-07 11:33:52 +0300

    InitCommit

---
+++ chat/Controllers/MainMenuController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chat.Controllers
+{
+    public class MainMenuController : Controller
+    {
+        public IActionResult Hello()
+        {
+            return View();
+        }
+    }
+}
