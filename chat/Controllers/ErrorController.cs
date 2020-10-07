fba420ccf3ab8f0c810cb610082e402966975afa
Viktor Marynyuk <viktor.marynyuk@gmail.com>
2020-10-07 12:34:08 +0300

    Added ErrorControler
    Custom 404 error page

---
+++ chat/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chat.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "404: Page not found";
+                    break;
+            }
+            return View("Error");
+        }
+    }
+}
--- chat/Startup.cs
+++ chat/Startup.cs
@@ -51,9 +51,7 @@ namespace chat
             }
             else
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseStatusCodePagesWithRedirects("/Error/{0}");
             }
 
             app.UseHttpsRedirection();
