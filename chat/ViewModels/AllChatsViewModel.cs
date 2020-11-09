af95295e355215c2d7bd93f772ce95cceb985f71
Viktor Marynyuk <viktor.marynyuk@gmail.com>
2020-11-09 18:18:06 +0200

    very small fixes

---
+++ chat/ViewModels/AllChatsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using chat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chat.ViewModels
+{
+    public class AllChatsViewModel
+    {
+        public List<AppUserChat> ChatList { get; set; }
+        public string ChatName { get; set; }
+    }
+}
